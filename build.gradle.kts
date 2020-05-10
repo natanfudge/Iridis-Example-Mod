3eff995f657591d69cdf7c49739df453f86c77fd
natanfudge <natan.lifsiz@gmail.com>
2020-05-11 01:40:10 +0300

    asm power

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,73 @@
 
-subprojects {
-    apply(plugin = "java")
+
+plugins {
+    java
+    id("fabric-loom") version "0.2.7-SNAPSHOT"
+}
 
 
-    group = rootProject.group
-    version = rootProject.version
+val commonCompileOnly = configurations.create("commonCompileOnly")
 
-    repositories {
-        mavenCentral()
-        mavenLocal()
+val commonImplementation = configurations.create("commonImplementation"){
+    extendsFrom(commonCompileOnly)
+}
+
+
+val minecraftVersion = "1.15.2"
+val yarnMappings = "1.15.2+build.14:v2"
+val loaderVersion = "0.7.8+build.189"
+
+repositories {
+    mavenLocal()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings("net.fabricmc:yarn:$yarnMappings")
+    modCompile("net.fabricmc:fabric-loader:$loaderVersion")
+
+    modImplementation("io.github.ladder:ladder-impl-fabric:1.0-SNAPSHOT")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.7.1+build.301-1.15")
+
+    commonCompileOnly(files("libs/ladder-stubs.jar"))
+    commonCompileOnly(files("libs/ladder-api-1.0-SNAPSHOT.jar"))
+
+}
+
+sourceSets {
+    val common = create("commonMain") {
+        compileClasspath += commonCompileOnly
+    }
+    create("fabricMain") {
+        compileClasspath += main.get().compileClasspath + common.output
+        runtimeClasspath += main.get().runtimeClasspath + common.output
+
+        resources {
+            source(common.resources)
+        }
     }
 
-    configure<JavaPluginConvention> {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+}
+
+
+configure<BasePluginConvention> {
+    archivesBaseName = "$archivesBaseName-mc$minecraftVersion"
+}
+
+tasks.named<Copy>("processResources") {
+    // this will ensure that this task is redone when the versions change.
+    inputs.property("version", version)
+
+    from(sourceSets["commonMain"].resources.srcDirs) {
+        include("fabric.mod.json")
+        expand("version" to version)
+    }
+
+    // copy everything else except the mod json
+    from(sourceSets["commonMain"].resources.srcDirs) {
+        exclude("fabric.mod.json")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,36 +1,17 @@
 pluginManagement {
     repositories {
         jcenter()
-
-        maven {
-            name = "Mixin maven"
-            url = uri("https://repo.spongepowered.org/maven" )}
         maven {
             name = "Fabric maven"
             url = uri("https://maven.fabricmc.net/")
         }
 
-        maven{
-            name = "Forge maven"
-            url = uri("https://files.minecraftforge.net/maven")
-        }
-
         gradlePluginPortal()
     }
 
-    resolutionStrategy {
-        eachPlugin {
-            if ("net.minecraftforge.gradle" == requested.id.id) {
-                useModule("net.minecraftforge.gradle:ForgeGradle:${requested.version}")
-            }
-            if("org.spongepowered.mixin" == requested.id.id){
-                useModule("org.spongepowered:mixingradle:${requested.version}")
-            }
-        }
-    }
 }
 
-rootProject.name = "Ladder-Example-Mod"
+rootProject.name = "Ladder-Pure-Fabric-Example-Mod"
 
 include("common")
 include("fabric")
